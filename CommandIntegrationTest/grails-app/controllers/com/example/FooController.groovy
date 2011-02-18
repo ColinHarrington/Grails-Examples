7fef28924bfc02c8fe9ee2b52baf59e6c530ea43
Colin Harrington <colin.harrington@gmail.com>
2011-02-18 11:41:35 -0600

    Updated the tests

--- CommandIntegrationTest/grails-app/controllers/com/example/FooController.groovy
+++ CommandIntegrationTest/grails-app/controllers/com/example/FooController.groovy
@@ -12,7 +12,6 @@ class FooCommandBean {
     String bar
 
     static constraints = {
-        // Password strength rule = any sequence of characters starting, at least one digit, at least one letter.
-        bar minSize:6,  matches: /^.*(?=.*\d)(?=.*[a-zA-Z]).*$/
+        bar minSize:6
     }
 }
--- CommandIntegrationTest/test/integration/com/example/FooCommandBeanTests.groovy
+++ CommandIntegrationTest/test/integration/com/example/FooCommandBeanTests.groovy
@@ -9,25 +9,11 @@ class FooCommandBeanTests extends GroovyTestCase {
         super.tearDown()
     }
 
-    void testPasswordStrength() {
-        def testBar = 'welcome1'
+    void testBar() {
+        def testBar = 'asdfghjkl'
         FooCommandBean bean = new FooCommandBean(bar:testBar)
 
         bean.validate()
         assertFalse bean.hasErrors()
-
-        foo = 'welcome'        // No number
-        bean = new FooCommandBean(bar:testBar)
-        bean.validate()
-        assertTrue bean.hasErrors()
-
-        foo = '12345$%^&*(@'        // No letter
-        bean = new FooCommandBean(bar:testBar)
-        bean.validate()
-        assertTrue bean.hasErrors()
-
-        bean.errors.allErrors.each {
-            println it
-        }
     }
 }
